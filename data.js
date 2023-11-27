60b51945c7d8c4c7f2b4a21209173017f1e60304
mehmetaligozbulut <mehmetgozbulut@gmail.com>
2023-11-27 11:39:24 +0300

    data description fix

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ module.exports = () => {
         name: 'Abant Doğal Kaynak Suyu 5 L',
         price: 13.50,
         imageUrl: 'icecek1.jpg',
-        description: 'idare eder 1',
+        description: 'Description 1',
         category: 'İçecek'
       },
       {
@@ -14,7 +14,7 @@ module.exports = () => {
         name: 'Sütaş Pratik Şişe Ayran 1 L',
         price: 23.50,
         imageUrl: 'icecek2.jpg',
-        description: 'idare eder 2',
+        description: 'Description 2',
         category: 'İçecek'
       },
       {
@@ -22,7 +22,7 @@ module.exports = () => {
         name: 'Sırma Doğal Maden Suyu 6x200 Ml Cam',
         price: 29.50,
         imageUrl: 'icecek3.jpg',
-        description: 'idare eder 3',
+        description: 'Description 3',
         category: 'İçecek'
       },
       {
@@ -30,7 +30,7 @@ module.exports = () => {
         name: 'Altınkılıç Kefir 1L',
         price: 39.50,
         imageUrl: 'icecek4.jpg',
-        description: 'idare eder 4',
+        description: 'Description 4',
         category: 'İçecek'
       },
       {
@@ -38,7 +38,7 @@ module.exports = () => {
         name: 'Kurukahveci Mehmet Efendi Türk Kahvesi 250 G',
         price: 99.90,
         imageUrl: 'icecek5.jpg',
-        description: 'idare eder 5',
+        description: 'Description 5',
         category: 'İçecek'
       },
       {
@@ -46,7 +46,7 @@ module.exports = () => {
         name: "Magnum Mini Badem 6'lı 345 Ml",
         price: 99.90,
         imageUrl: 'dondurma1.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Dondurma'
       },
       {
@@ -54,7 +54,7 @@ module.exports = () => {
         name: "Carte d'Or Barista Americano - Latte - Madagaskar",
         price: 89.90,
         imageUrl: 'dondurma2.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Dondurma'
       },
       {
@@ -62,7 +62,7 @@ module.exports = () => {
         name: "Carte d'Or Selection Çikolata Karnavalı 800 ml",
         price: 92.50,
         imageUrl: 'dondurma3.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Dondurma'
       },
       {
@@ -70,7 +70,7 @@ module.exports = () => {
         name: 'Algida Maraş Usulü Bol Kaymak 500 Ml',
         price: 74.90,
         imageUrl: 'dondurma4.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Dondurma'
       },
       {
@@ -78,7 +78,7 @@ module.exports = () => {
         name: 'Migros İç Ceviz 150 G',
         price: 48.50,
         imageUrl: 'atistirmalik1.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Atıştırmalık'
       },
       {
@@ -86,7 +86,7 @@ module.exports = () => {
         name: 'Nestle Damak Antep Fıstıklı Sütlü Kare Çikolata 60 G',
         price: 20.93,
         imageUrl: 'atistirmalik2.jpg',
-        description: 'idare eder 6',
+        description: 'Description 6',
         category: 'Atıştırmalık'
       },
     ],
